3fae25a9cc5cfa52d41b0754ae2f52e66e838231
pereiramemo <epereira@epereiramemo-OptiPlex-7010.(none)>
2015-06-24 12:13:26 +0200

    update

--- dev/alias_functions.sh
+++ dev/alias_functions.sh
@@ -1,5 +1,5 @@
 drm() { sudo docker rm $(docker ps -q -a); }
 drmi() { sudo docker rmi $(docker images -q); }
-dirun() { sudo docker run -t -i --rm "$1" bash; }
+dirun() { sudo docker run --net=host -t -i --rm "$1" bash; }
 dimrun() { sudo docker run --net=host -ti --volume="$1" "$2" bash; }
 dbuild() { sudo docker build -t="$1" .; }
